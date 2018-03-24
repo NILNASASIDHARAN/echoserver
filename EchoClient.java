5464ee90fa81b1d25d8d209426ac922a9b0476ca
Joshua <pkjoshua123@gmail.com>
2018-03-24 11:54:29 +0530

    New Client

--- EchoClient.java
+++ EchoClient.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.net.*;
 
-class TCPClient {
+class EchoClient {
  public static void main(String argv[]) throws Exception {
   String sentence;
   String modifiedSentence;
