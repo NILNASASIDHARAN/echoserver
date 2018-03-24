6991e5028a5620a6b3345cc7630d5893edc1dbfb
Joshua <pkjoshua123@gmail.com>
2018-03-24 11:59:07 +0530

    Echoserver Completed

--- EchoClient.java
+++ EchoClient.java
@@ -7,7 +7,7 @@ class EchoClient {
   String modifiedSentence;
   BufferedReader inFromUser = new BufferedReader(new InputStreamReader(System.in));
   Socket clientSocket = new Socket("localhost", 6789);
-  while(true){
+  
   DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
   BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
   sentence = inFromUser.readLine();
@@ -16,7 +16,7 @@ class EchoClient {
   System.out.println("FROM SERVER: " + modifiedSentence);
   
   clientSocket.close();
-  }
+  
   
  }
 }
