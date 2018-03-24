60b7f2c911c66906d6af279c3ec6cb6616ba618b
Lubna <lubilubna16@gmail.com>
2018-03-24 11:23:55 +0530

    Done

--- EchoClient.java
+++ EchoClient.java
@@ -1,36 +1,30 @@
-import java.net.Socket;
+import java.io.*;
+import java.net.*;
 
-public class EchoClient {
-
-    public static void main(String[] args) throws Exception {
-        String screenName = args[0];
-        String host       = args[1];
-        int port          = 4444;
-
-        // connect to server and open up IO streams
-        Socket socket = new Socket(host, port);
-        In     stdin  = new In();
-        In     in     = new In(socket);
-        Out    out    = new Out(socket);
-        System.err.println("Connected to " + host + " on port " + port);
-
-        // read in a line from stdin, send to server, and print back reply
-        while (!stdin.hasNextLine()) {
-
-            // read line of client
-            String s = stdin.readLine();
-
-            // send over socket to server
-            out.println("[" + screenName + "]: " + s);
-
-            // get reply from server and print it out
-            StdOut.println(in.readLine());
+public class EchoClient
+{
+    public static void main(String[] args)
+    {
+        try
+        {
+            Socket s = new Socket("my static ip address here", 3000);
+            BufferedReader r = new BufferedReader(new InputStreamReader(s.getInputStream()));
+            PrintWriter w = new PrintWriter(s.getOutputStream(), true);
+            BufferedReader con = new BufferedReader(new InputStreamReader(System.in));
+            String line;
+            do
+            {
+                line = r.readLine();
+                if ( line != null )
+                    System.out.println(line);
+                line = con.readLine();
+                w.println(line);
+            }
+            while ( !line.trim().equals("bye") );
         }
-
-        // close IO streams, then socket
-        System.err.println("Closing connection to " + host);
-        out.close();
-        in.close();
-        socket.close();
+        catch (Exception err)
+        {
+            System.err.println(err);
+        }
     }
 }
