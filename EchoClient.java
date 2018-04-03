35567900f04e6fe16186d6afac491481bc1b0d54
LubnaYousaf <36188487+LubnaYousaf@users.noreply.github.com>
2018-04-03 13:20:25 +0530

    Update EchoClient.java

--- EchoClient.java
+++ EchoClient.java
@@ -10,13 +10,11 @@ class EchoClient {
   
   DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
   BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+  while(true){
   sentence = inFromUser.readLine();
   outToServer.writeBytes(sentence + '\n');
   modifiedSentence = inFromServer.readLine();
   System.out.println("FROM SERVER: " + modifiedSentence);
-  
-  clientSocket.close();
-  
-  
+  }   
  }
 }
